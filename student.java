e9b1989625843019be76618acd680ffbb4052899
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-03-13 09:02:29 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,13 @@
+package finalstatic;
+
+public class student {
+int a;
+static int b;
+student (){
+	b++;
+}
+public void showdata() {
+	System.out.println("value a = "+a);
+	System.out.println("value b = "+ b);
+}
+}
