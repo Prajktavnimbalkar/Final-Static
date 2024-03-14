bc0aa4b8c3618356b0dc91f780f00e1b3b244a18
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-03-14 09:15:12 +0530

    Add files via upload

---
+++ studentmain.java
@@ -0,0 +1,11 @@
+package finalstatic;
+
+public class studentmain {
+	public static void main(String[] args) {
+		student s1 = new student();
+		s1.showdata();
+		student s2 = new student();
+		s2.showdata();
+	}
+
+}
